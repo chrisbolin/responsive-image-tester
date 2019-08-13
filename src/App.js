1c93822a9f388266b7a3bce5b12f4ca2253349a3
Chris Bolin <bolin.chris@gmail.com>
2019-08-13 09:44:26 -0600

    styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 
 const srcSet = (widths, prefix) =>
   widths.map(width => `/${prefix}${width}.jpg ${width}w`).join(",\n");
@@ -9,12 +9,45 @@ const verticalSrcSet = srcSet([600, 900, 1280], "v");
 const backup = "/original.jpg";
 const alt = "sunset";
 
-const Home = () => <div>home</div>;
+const HomeLink = () => (
+  <p>
+    <Link to="/">home</Link>
+  </p>
+);
 
-const SrcSet = () => <img srcSet={horizontalSrcSet} src={backup} alt={alt} />;
+const Home = () => (
+  <div>
+    <nav>
+      <p>
+        <Link to="/srcset">basic responsive image</Link> — img element with
+        srcset attribute, client JavaScript rendering
+      </p>
+      <p>
+        <Link to="/picture">advanced responsive image</Link> — picture with
+        source elements and srcset attribute, client JavaScript rendering
+      </p>
+      <p>
+        <a href="/server/srcset">basic responsive image, server only</a> — img
+        element with srcset attribute, no client JavaScript
+      </p>
+      <p>
+        <a href="/server/picture">advanced responsive image, server only</a> —
+        picture with source elements and srcset attribute, no client JavaScript
+      </p>
+    </nav>
+  </div>
+);
+
+const SrcSet = () => (
+  <div>
+    <HomeLink />
+    <img srcSet={horizontalSrcSet} src={backup} alt={alt} />
+  </div>
+);
 
 const Picture = () => (
   <div>
+    <HomeLink />
     <picture>
       <source media="(max-width: 499px)" srcSet={verticalSrcSet} />
       <source media="(min-width: 500px)" srcSet={horizontalSrcSet} />
