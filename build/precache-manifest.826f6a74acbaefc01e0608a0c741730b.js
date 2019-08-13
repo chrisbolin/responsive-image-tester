4eec3026d7ad9d5f70e6da897d28062e7946742a
Chris Bolin <bolin.chris@gmail.com>
2019-08-13 09:46:55 -0600

    add build

---
+++ build/precache-manifest.826f6a74acbaefc01e0608a0c741730b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9afad16fa2c546717e898f09eaacd4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6fb36395e360e1984ce",
+    "url": "/static/css/main.07d63909.chunk.css"
+  },
+  {
+    "revision": "596526b5b3db6271abf6",
+    "url": "/static/js/2.4627fce9.chunk.js"
+  },
+  {
+    "revision": "b6fb36395e360e1984ce",
+    "url": "/static/js/main.79309b24.chunk.js"
+  },
+  {
+    "revision": "61fb7cb0a9c78b84b0da",
+    "url": "/static/js/runtime~main.34ad7064.js"
+  }
+]);
